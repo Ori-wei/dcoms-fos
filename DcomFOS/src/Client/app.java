f2297b850c43306eac8b4a771f7347b41292d8f9
haoming-mmu <haoming.kong9@gmail.com>
2023-08-18 11:40:39 +0800

    7th commit. completed menu, product detail and cart module

--- DcomFOS/src/Client/app.java
+++ DcomFOS/src/Client/app.java
@@ -28,6 +28,8 @@ public class app {
 //        }
         
         //HM
-        MenuFrameClient.createAndShowGUI();
+        //global variable
+        int userID=2;
+        MenuFrameClient.createAndShowGUI(userID);
     }
 }
